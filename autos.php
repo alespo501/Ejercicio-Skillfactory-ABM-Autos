ff22a802d811da81667d1293aafd1162e3b553d6
alespo501 <72363930+alespo501@users.noreply.github.com>
2020-10-12 21:25:52 -0300

    Add files via upload

---
+++ autos.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `blog_master`
+//
+
+// `blog_master`.`autos`
+$autos = array(
+  array('id' => '1','marca' => 'Chevrolet','modelo' => 'Cruze 1.8 LTZ','año' => '2015','color' => 'Blanco'),
+  array('id' => '2','marca' => 'Chevrolet','modelo' => 'Agile LTZ','año' => '2013','color' => 'Negro'),
+  array('id' => '3','marca' => 'Toyota','modelo' => 'Etios XLS 1.5 ','año' => '2018','color' => 'Blanco'),
+  array('id' => '4','marca' => 'Nissan','modelo' => 'Patfinder 2.2 4x4','año' => '2005','color' => 'Rojo'),
+  array('id' => '5','marca' => 'Ford','modelo' => 'Fiesta LTX 1.6','año' => '2011','color' => 'Gris Plata'),
+  array('id' => '6','marca' => 'Renault','modelo' => 'Twingo  S1.0','año' => '1998','color' => 'Celeste'),
+  array('id' => '7','marca' => 'Renault','modelo' => 'Kwid XS 1.0','año' => '2019','color' => 'Bordeaux'),
+  array('id' => '8','marca' => 'Volkswagen','modelo' => 'UP! Take 1.0','año' => '2019','color' => 'Verde Metalizado'),
+  array('id' => '9','marca' => 'Nissan','modelo' => 'March Sensei 1.6','año' => '2017','color' => 'Gris metalizado')
+);
